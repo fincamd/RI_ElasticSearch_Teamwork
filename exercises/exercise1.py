1dff5bf7694bab55a26d43392c419e4163cfd947
Daniel Finca <37817126+fincamd@users.noreply.github.com>
2019-12-10 18:27:48 +0100

    Primer paso para el ejercicio 1

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -10,7 +10,6 @@ from elasticsearch.client import IndicesClient
 
 ppPrinter = pprint.PrettyPrinter()
 searchEngine = Elasticsearch()
-indicesClient = IndicesClient(searchEngine)
 
 
 def main():
@@ -18,35 +17,35 @@ def main():
 
     # domains = searchEngine.search(
     #     index="reddit-mentalhealth",
-    #     body={
-    #         "query": {"match": {"selftext": {"query": "depression"}}},
-    #         "_source": [
-    #             "category",
-    #             "distinguished",
-    #             "downs",
-    #             "ups",
-    #             "guilded",
-    #             "saved",
-    #             "subreddit_id",
-    #             "subreddit_subs",
-    #             "title",
-    #             "domain",
-    #             "subreddit",
-    #             "selftext",
-    #         ],
-    #         "size": 10,
-    #         "track_total_hits": True,
-    #     },
+        body={
+            "query": {"match": {"selftext": {"query": "depression"}}},
+            "_source": [
+                "category",
+                "distinguished",
+                "downs",
+                "ups",
+                "guilded",
+                "saved",
+                "subreddit_id",
+                "subreddit_subs",
+                "title",
+                "domain",
+                "subreddit",
+                "selftext",
+            ],
+            "size": 10,
+            "track_total_hits": True,
+        },
     # )
 
-    domains = searchEngine.search(
-        index="reddit-mentalhealth",
-        body={
-            "size": 0,
-            "query": {"match": {"selftext": "depression"}},
-            "aggs": {"mostCommonTerms": {"terms": {"field": "selftext", "size": 20}}},
-        },
-    )
+    # domains = searchEngine.search(
+    #     index="reddit-mentalhealth",
+    #     body={
+    #         "size": 0,
+    #         "query": {"match": {"selftext": "depression"}},
+    #         "aggs": {"mostCommonTerms": {"terms": {"field": "selftext", "size": 20}}},
+    #     },
+    # )
 
     # domains = searchEngine.search(
     #     index="reddit-mentalhealth",
@@ -62,17 +61,6 @@ def main():
     #             }
     #         }
     #     }
-    # )
-
-    # indicesClient.put_settings(
-    #     body={
-    #         "settings": {
-    #             "analysis": {
-    #                 "analyzer": {"my_stopper": {"type": "stop", "stopwords": "english"}}
-    #             }
-    #         }
-    #     },
-    #     index="reddit-mentalhealth",
     # )
 
     ppPrinter.pprint(domains)
---
+++ exercises/query.py
@@ -0,0 +1,57 @@
+# Para poder usar la función print e imprimir sin saltos de línea
+from __future__ import print_function
+
+import json  # Para poder trabajar con objetos JSON
+import pprint  # Para poder hacer uso de PrettyPrinter
+import sys  # Para poder usar exit
+
+from elasticsearch import Elasticsearch
+from elasticsearch.client import IndicesClient
+
+ppPrinter = pprint.PrettyPrinter()
+searchEngine = Elasticsearch()
+
+relevantTerms = []
+
+
+def main():
+
+    results = searchEngine.search(
+        index="reddit-mentalhealth4",
+        body={
+            "size": 0,
+            "query": {
+                "query_string": {"default_field": "selftext", "query": "alcoholism"}
+            },
+            "aggs": {
+                "por_palabbres": {
+                    "significant_text": {
+                        "field": "selftext",
+                        "size": 100,
+                        "chi_square": {},
+                    },
+                },
+            },
+        },
+    )
+
+    keys = results["aggregations"]["por_palabbres"]["buckets"]
+
+    for key in keys:
+        term = key["key"].replace("_", "").strip()
+        if relevantTerms.count(term) == 0:
+            relevantTerms.append(term)
+            print(term)
+
+    # ppPrinter.pprint(results)
+
+    rows = list(relevantTerms)
+    columns = list(relevantTerms)
+
+    for row in rows:
+        for column in columns:
+            print(row, column)
+
+
+if __name__ == "__main__":
+    main()
