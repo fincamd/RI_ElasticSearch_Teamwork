7cbe7d01c61a2543297c326c1f54ff5c01df7f3b
Daniel Finca <37817126+fincamd@users.noreply.github.com>
2019-12-13 18:08:05 +0100

    Ejercicio 3 acabado y con un par de archivos creados

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -5,6 +5,7 @@ import json  # Para poder trabajar con objetos JSON
 import pprint  # Para poder hacer uso de PrettyPrinter
 import sys  # Para poder usar exit
 import requests
+import random
 
 from elasticsearch import Elasticsearch
 
@@ -14,26 +15,46 @@ _WIKIDATA_DICTIONARY_URL = "https://www.wikidata.org/wiki/Special:EntityData/%IT
 ppPrinter = pprint.PrettyPrinter()
 searchEngine = Elasticsearch()
 
+debugLog = []
+
+def isMedicine(word):
+    # Get the medicine json from wikidata
+    articleIndexJson = requests.get(_WIKIDATA_INDEX_URL.replace("%SEARCH%", word)).json()
+    articleIndexJson = articleIndexJson["search"]
+    # Take the Q from the results json
+    for item in articleIndexJson:
+        itemId = item["title"]
+        # Query wikidata again to get json using Q param
+        wikidataItemsIndex = requests.get(_WIKIDATA_DICTIONARY_URL.replace("%ITEM_ID%", itemId)).json()
+        wikidataItemsIndex = wikidataItemsIndex["entities"][itemId]["claims"]
+        # Take P31, look for Q for medication/medicine and for pharmaceutical product
+        if wikidataItemsIndex.get("P31") != None:
+            instanceOfProperty = wikidataItemsIndex["P31"]
+            for superclass in instanceOfProperty:
+                resultId = superclass["mainsnak"]["datavalue"]["value"]["id"]
+                if resultId == "Q12140":
+                    return True
+    return False
+
+
 def main():
     results = searchEngine.search(
     index="reddit-mentalhealth4",
     body={
         "size": 0,
-        "query": { "match": { "selftext": "prescribed"} },
+        "query": { "match": { "selftext": "prescribe"} },
         "aggs": {
             "medicines": { 
                 "significant_terms": { 
                     "field": "selftext",
-                    "exclude": ".*(prescri|doctor|med|take|dose|psychiatrist|[0-9]+|mg).*",
-                    "size": 500,
+                    # "exclude": ".*(prescri|doctor|med|take|dose|psychiatrist|[0-9]+|mg).*",
+                    "size": 100,
                 },
             },
         }
     },
     request_timeout=30
     )
-
-    # ppPrinter.pprint(results)
 
     results = results["aggregations"]["medicines"]["buckets"]
 
@@ -49,31 +70,19 @@ def main():
     for term in singleTerms:
         totalItems += 1
         if isMedicine(term):
-            print(term)
             actualMedicines += 1
-        print("The percentage of medicines from extracted terms is: ", actualMedicines / totalItems * 100, "%")
+        print("/-------------------------------------------------------------/")
+        print("Term just processed -->", term)
+        print("Terms ratio --> Total terms:", totalItems, "|| Actual medicines:", actualMedicines)
+        print("The percentage of medicines from extracted terms is: %.4f%s" %(actualMedicines / totalItems * 100, "%"))
+        debugLog.append("/-------------------------------------------------------------/\n")
+        debugLog.append("Term just processed --> " + str(term) + "\n")
+        debugLog.append("Terms ratio --> Total terms: " + str(totalItems) + " || Actual medicines: " + str(actualMedicines) + "\n")
+        formattedPercentageLog = ("The percentage of medicines from extracted terms is: %.4f%s" %(actualMedicines / totalItems * 100, "%\n"))
+        debugLog.append(formattedPercentageLog)
 
-    with open("medicationFound.json", "wt") as dumpFile:
-        pass
-
-def isMedicine(word):
-    ids = []
-    # Get the medicine json from wikidata
-    articleIndexJson = requests.get(_WIKIDATA_INDEX_URL.replace("%SEARCH%", word)).json()
-    articleIndexJson = articleIndexJson["search"]
-    # Take the Q from the results json
-    for item in articleIndexJson:
-        itemId = item["title"]
-        # Query wikidata again to get json using Q param
-        wikidataItemsIndex = requests.get(_WIKIDATA_DICTIONARY_URL.replace("%ITEM_ID%", itemId)).json()
-        wikidataItemsIndex = wikidataItemsIndex["entities"][itemId]["claims"]
-        # Take P31, look for Q for medication/medicine and for pharmaceutical product
-        if wikidataItemsIndex.get("P31") != None:
-            instanceOfProperty = wikidataItemsIndex["P31"]
-            for superclass in instanceOfProperty:
-                ids.append(superclass["mainsnak"]["datavalue"]["value"]["id"])
-    return ids.count("Q12140") > 0
-
+    with open("medicationFound_" + str(random.randint(0, sys.maxsize)) + ".json", "wt") as dumpFile:
+        dumpFile.writelines(debugLog)
 
 if __name__ == "__main__":
     main()
